7535a470ec0e491a1df47f858061120e8f9779ad
Evgeny <chapayevkrim@rambler.ru>
2023-04-26 21:47:17 +0300

    Created Django project

---
+++ drfsite/women/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+# import django
+# django.setup()
+
+
+class WomenConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'women'
+
+
