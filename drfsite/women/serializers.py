effab471f58d78571bfc041f2162e43cd31827b1
Evgeny <chapayevkrim@rambler.ru>
2023-04-27 23:23:37 +0300

    Added serializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,9 +1,22 @@
+import io
 from rest_framework import serializers
-
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Women
 
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255) # Field - это валидаторы сериализаторов
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField() #
+        # отличие - в models.py в class Momen
+        # cat_id определен как ForeignKey, то
+        # в сериализаторе прописываем cat_id конкретным образом
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -8,20 +8,42 @@ from .serializers import WomenSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class WomenAPIView(APIView):
-    def get(self, request): # простейший get-запрос без сериализатора
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+    def get(self, request):
+        w = Women.objects.all() # список статей получаем как набор queryset
+        return Response({'posts': WomenSerializer(w, many=True).data})
+        # и с помощью сериализатора WomenSerializer. many=True - сериализатор обрабатывает список записей
+        # data - словарь преобразованных данных из таблицы Women
 
     def post(self, request):
-        post_new = Women.objects.create( # objects - это менеджер
-            title=request.data['title'], #title берет из коллекции request значение data по ключу (поле) ['title']
-            content=request.data['content'], #
-            cat_id=request.data['cat_id'] #
+        serializer = WomenSerializer(data=request.data) # проверка, если данные в БД отсутствуют
+        serializer.is_valid(raise_exception=True)
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(post_new)}) # получить значения, что именно добавили в БД
-                        # ключ 'post', а значение - функция model_to_dict, который преобразовывает объект класса Women
-                        # из модели Джанго в словарь
+
+        return Response({'post': WomenSerializer(post_new).data})
+
+
+# class WomenAPIView(APIView):
+#     def get(self, request): # простейший get-запрос без сериализатора
+#         lst = Women.objects.all().values()
+#         return Response({'posts': list(lst)})
+#
+#     def post(self, request):
+#         post_new = Women.objects.create( # objects - это менеджер
+#             title=request.data['title'], #title берет из коллекции request значение data по ключу (поле) ['title']
+#             content=request.data['content'], #
+#             cat_id=request.data['cat_id'] #
+#         )
+#         return Response({'post': model_to_dict(post_new)}) # получить значения, что именно добавили в БД
+#                         # ключ 'post', а значение - функция model_to_dict, который преобразовывает объект класса Women
+#                         # из модели Джанго в словарь
+
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
+
