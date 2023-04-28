ef90e47e66123dae32441a90f0af6c8964b81aee
Evgeny <chapayevkrim@rambler.ru>
2023-04-28 09:28:01 +0300

    Added methods save(), create() и update() of serializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -17,6 +17,22 @@ class WomenSerializer(serializers.Serializer):
     time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField() #
-        # отличие - в models.py в class Momen
-        # cat_id определен как ForeignKey, то
+        # отличие - в models.py в class Momen cat_id определен как ForeignKey, то
         # в сериализаторе прописываем cat_id конкретным образом
+
+
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+            # instance это ссылка на объект модели Women
+            # validated_data это словарь из проверенных данных который нужно изменить
+        instance.title = validated_data.get("title", instance.title)
+            # "title" - ключ из словаря, а если этого ключа нет, то:
+            # instance.title - возвращаем ключ из словаря модели Women
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+        instance.save()
+        return instance # возвращаем объект instance
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,7 +1,8 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework import generics # , request
+from rest_framework.decorators import api_view
 
-from rest_framework import generics
 from .models import Women
 from .serializers import WomenSerializer
 
@@ -10,6 +11,7 @@ from rest_framework.views import APIView
 
 
 class WomenAPIView(APIView):
+
     def get(self, request):
         w = Women.objects.all() # список статей получаем как набор queryset
         return Response({'posts': WomenSerializer(w, many=True).data})
@@ -19,13 +21,11 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data) # проверка, если данные в БД отсутствуют
         serializer.is_valid(raise_exception=True)
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
+        serializer.save() # этот метод save() автоматически вызовет метод create из serializers.py
 
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data}) # serializer.data - коллекция которую возвратит метод create
+        # serializer.data - мы не создаем новый словарь, а воспользуемся уже созданныым на строке 22
+        # return Response({'post': WomenSerializer(post_new).data})
 
 
 # class WomenAPIView(APIView):
@@ -47,3 +47,46 @@ class WomenAPIView(APIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
 
+    # @api_view(['GET', 'POST', 'PUT'])
+    def put(self, request, *args, **kwargs): # что положим (изменим)
+        pk = kwargs.get('pk', None)
+            # pk - идентификатор записи, которую нужно поменять
+            # обращаемся к словарю kwargs, берем (.get) из него ключ pk
+            # а если ключ там не присутствует, возвращаем None
+        if not pk: # если ключ там не присутствует, то
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+                # извлекаем запись из модели Women по ключу pk
+        except: # иначе:
+            return Response({"error": "Object does not exists"})
+
+            # и когда мы получили и ключ и запись по этому ключу, то создаем сериализатор
+        serializer = WomenSerializer(data=request.data, instance=instance)
+            # request.data - те данные которые нам нужно изменить
+            # instance - объект, - та запись, которую мы собираемся поменять
+        serializer.is_valid(raise_exception=True)
+        serializer.save() # здесь метод save автоматически вызовет метод update из serializer.py
+            # после того как поменяли запись, отправляем клиенту запись в виде JSON-строки
+        return Response({"post": serializer.data}) # serializer.data - те данные,которые были изменены
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)  # получили ключ
+        if not pk: # а если его нет,то:
+            return Response({"error": "Method DELETE not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+                # извлекаем запись из модели Women по ключу pk
+        except: # иначе:
+            return Response({"error": "Object does not exists"})
+
+            # и когда мы получили и ключ и запись по этому ключу, то создаем сериализатор
+        serializer = WomenSerializer(data=request.data, instance=instance)
+            # request.data - те данные которые нам нужно изменить
+            # instance - объект, - та запись, которую мы собираемся поменять
+        serializer.is_valid(raise_exception=True)
+        instance.delete()
+        # после того как поменяли запись, отправляем клиенту запись в виде JSON-строки
+        return Response({"post": "delete post " + str(pk)}) # и возвратить этот ответ клиенту
