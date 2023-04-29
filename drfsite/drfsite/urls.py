3c296a92544957385a41842c149631426a9c5534
Evgeny <chapayevkrim@rambler.ru>
2023-04-29 17:19:24 +0300

    Added SimpleRouter, DefaultRouter

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,36 +1,42 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import MyCustomRouter
 
 from women.views import *
 from rest_framework import routers
 
-# создаем роутер
-router = routers.SimpleRouter() # из ветки routers мы обратимся к классу SimpleRouter()
-# связываем этот роутер с вьюсетом WomenViewSet.
-# Делается это с помощью метода register
-router.register(r'women', WomenViewSet)
-    # регистрируем метод вьюсЕта - router.register
-    # r - префикс для набора маршрутов
-    # WomenViewSet - класс вьюсета
+# class MyCustomRouter(routers.SimpleRouter): # создание собственного класса роутера (взят из документации)
+#     routes = [ # routes - список из маршрутов
+#         # routers.Route 1-ый маршрут - определяет список статей
+#         routers.Route(url=r'^{prefix}$', # ^{prefix}$ - регулярное выражение
+                                            # надо запомнить, что адреса здесь определены без обратного слеша
+#                       mapping={'get': 'list'}, # связывает тип запроса с соответствующим методом ViewSet
+#                       name='{basename}-list',
+#                       detail=False, # список или отдельная запись
+#                       initkwargs={'suffix': 'List'}), # при срабатывании маршрута передает аргументы
+#         # дополнительные аргументы для коллекции kwargs, которые передаются конкретному представлению при срабатывании маршрута.
+#
+#         # routers.Route - 2-ой маршрут - определяет конкретную статью по ее идентификатору
+#         routers.Route(url=r'^{prefix}/{lookup}$',
+#                       mapping={'get': 'retrieve'},
+#                       name='{basename}-detail',
+#                       detail=True,
+#                       initkwargs={'suffix': 'Detail'})
+#     ]
 
-# до роутера
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
-#     # для метода 'get прописываем дополнительно словарь list
-#     # В частности, метод as_view({'get': 'list'}) связывает GET-запрос с возвратом клиенту списка записей из модели таблицы БД.
-#     # А метод as_view({'put': 'update'}) связывает PUT-запрос с методом update() для изменения текущей записи.
-#     path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
-# ]
 
-# с роутером
+# router = routers.DefaultRouter()
+# # router.register(r'women', WomenViewSet)
+# router.register(r'women', WomenViewSet, basename='women') # Но, так как мы используем роутер ,
+#                                                         # то нужно прописать параметр basename:
+# print(router.urls)
+
+router = MyCustomRouter()
+# router.register(r'women', WomenViewSet)
+router.register(r'women', WomenViewSet, basename='women')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),   # http://127.0.0.1:8000/api/v1/women/
-    # коллекция маршрутов urlpatterns, которые создал роутер
-    # include - включая
-    # когда мы зарегистрировали роутер стр. 12, то в роутере автомматически генерируется коллекция (router.urls)
-    # как формируется маршрут:
-    # стр. 30 сначала 'api/v1/', потом (смотреть в конце строки 30) - /women/
+
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,9 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics # , request
-from rest_framework.decorators import api_view
+from rest_framework import generics, mixins  # , request
+from rest_framework.decorators import api_view, action
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 
 from rest_framework.response import Response
@@ -12,27 +12,65 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 
 
-class WomenViewSet(viewsets.ModelViewSet): # заменяет все 3 класса  WomenAPIList, WomenAPIUpdate, WomenAPIDetailView
-    queryset = Women.objects.all()
+# class WomenViewSet(viewsets.ModelViewSet):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+
+# # с декоратором @action
+# class WomenViewSet(mixins.RetrieveModelMixin,
+#                    mixins.UpdateModelMixin,
+#                    mixins.ListModelMixin,
+#                    viewsets.GenericViewSet):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#     # @action(methods=['get'], detail=False)
+#     # def category(self, request):
+#     #     cats = Category.objects.all()
+#     #     return Response({'cats': [c.name for c in cats]})
+#
+#
+#     # допустим, нам нужно выводить список категорий. добавляем декоратор @action
+#     @action(methods=['get'], detail=True) # methods - поддерживаемые методы
+#     # detail=False - список категорий
+#     # detail=True - только одна запись
+#     # def category(self, request): # метод придумываем сами
+#     def category(self, request, pk=None): # если появляется параметр pk,
+#                                             # то мы выдает список из одной конкретной записи, пример:
+#                                             # http://127.0.0.1:8000/api/vcats.name1/women/1/category/
+#                                             # НАВЕСТИ КУРСОР НА АДРЕС НА СТРОКЕ ВЫШЕ И CTRL+v -
+#                                             # откроется новое окно в браузере # n
+#         cats = Category.objects.get(pk=pk)
+#         return Response({'cats': cats.name}) # возвращаем в JSON формате
+#                                             # cats.name - название категории
+
+
+                                        #  Метод get_queryset
+# ------------------- КОГДА НУЖНО ВОЗВРАЩАТЬ НЕ ВСЕ ЗАПИСИ, А ТОЛЬКО НУЖНЫЕ ---------------------
+
+class WomenViewSet(mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    #  queryset = Women.objects.all() # чтобы вывести первые 3 записи, а если мы применяем функцию get_queryset,
+                                    # строку queryset = Women.objects.all() убрать
     serializer_class = WomenSerializer
 
+    # def get_queryset(self): # возвращать список (у нас это список статей из БД)
+    #     return Women.objects.all()[:3]
 
-# class WomenViewSet(viewsets.ReadOnlyModelViewSet): # есть еще класс ReadOnlyModelViewSet,
-#                                                     # который позволяет только читать
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
 
-# class WomenAPIList(generics. ListCreateAPIView):
-#     queryset = Women.objects.all() # get запрос - получить список
-#     serializer_class = WomenSerializer # post - добавлять новую запись
-#
-#
-# class WomenAPIUpdate(generics.UpdateAPIView): # put запрос - изменить список
-#     queryset = Women.objects.all() # здесь клиенту будет отправлена только одна измененная запись
-#                                     # а не весь список, т.к. это ленивый запрос
-#     serializer_class = WomenSerializer
-#
-# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     # делает все: get, put, patch, delete
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+    def get_queryset(self): # возвращать только одну запись
+        pk = self.kwargs.get("pk") # kwargs - локальная коллекция ключей
+        if not pk:
+            return Women.objects.all()[:3]
+        return Women.objects.filter(pk=pk)# если появляется параметр pk, то мы выдает список из одной конкретной записи,
+                                            # а иначе – список из первых трех
+
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
